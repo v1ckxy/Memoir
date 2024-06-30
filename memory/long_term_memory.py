ebb20c39c08936ae2bf8760c01e7d89105fe78c4
v1ckxy <v1ckxy@proton.me>
2024-07-01 01:44:49 +0200

    LTM and RagDataMemory classes> Check if qdrant.collection exists before trying to create a new one. Saves downloaded embedder model to /storage Checks if embedder exists under /storage then tries to load it. Improved readability. Modified verbose mode messages.

--- memory/long_term_memory.py
+++ memory/long_term_memory.py
@@ -10,8 +10,10 @@ additional info here https://python-client.qdrant.tech/
 
 """
 
+import os
 import random
 from datetime import datetime, timedelta
+from modules.logging_colors import logger
 from qdrant_client import models, QdrantClient
 from qdrant_client.http.models import PointStruct
 from html import escape
@@ -19,49 +21,81 @@ from sentence_transformers import SentenceTransformer
 
 
 class LTM():
-    def __init__(self,
-                 collection,
-                 ltm_limit,
-                 verbose=False,
-                 embedder='all-mpnet-base-v2',
-                 address='localhost',
-                 port=6333
-                 ):
+    def __init__(
+            self,
+            collection,
+            ltm_limit,
+            verbose=False,
+            embedder='all-mpnet-base-v2',
+            address='localhost',
+            port=6333,
+            device='cuda'
+    ):
 
         self.verbose = verbose
         if self.verbose:
-            print("initiating verbose debug mode.............")
+            logger.debug(f'[Memoir] [{self.__class__.__name__}] Verbose mode enabled.')
         self.collection = collection
         self.ltm_limit = ltm_limit
-        # print("LTM LIMIT:" + str(ltm_limit))
+        if self.verbose:
+            logger.debug(f"[Memoir] [{self.__class__.__name__}] LIMIT: {str(ltm_limit)}")
         self.address = address
         self.port = port
         if self.verbose:
-            print(f"addr:{self.address}, port:{self.port}")
+            logger.debug(f'[Memoir] [{self.__class__.__name__}] [QDRANT] addr:{self.address}, port:{self.port}')
 
         self.embedder = embedder
-        self. encoder = SentenceTransformer(self.embedder)
+        self.device = device
+        # Check if specified embedder is already downloaded to load it from file
+        filePath = os.path.dirname(os.path.realpath(__file__))
+        basePath = os.path.abspath(os.path.join(filePath, os.pardir))
+        embedderSavePath = os.path.join(
+            basePath + os.sep,
+            "storage" + os.sep,
+            "models" + os.sep,
+            self.embedder
+        )
+        embedderSaveFile = os.path.join(
+            embedderSavePath + os.sep,
+            "model.safetensors"
+        )
+        if os.path.exists(embedderSavePath) and os.path.isfile(embedderSaveFile):
+            if self.verbose:
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] embedder {self.embedder} found in {embedderSavePath}, will try to load from disk.')
+            try:
+                self.encoder = SentenceTransformer(embedderSavePath)  # , device=self.device)
+            except Exception as e:
+                logger.error(f'[Memoir] [{self.__class__.__name__}] There was an error while loading {self.embedder}, please check.')
+        else:
+            if self.verbose:
+                logger.warning(f'[Memoir] [{self.__class__.__name__}] embedder {self.embedder} NOT FOUND in {embedderSavePath}, will be downloaded and saved to disk.')
+            self.encoder = SentenceTransformer(self.embedder)  # , device=self.device)
+            self.encoder.save(embedderSavePath)
+
         self.qdrant = QdrantClient(self.address, port=self.port)
         self.create_vector_db_if_missing()
 
     def create_vector_db_if_missing(self):
-        try:
-            self.qdrant.create_collection(
-                collection_name=self.collection,
-                vectors_config=models.VectorParams(
-                    size=self.encoder.get_sentence_embedding_dimension(),
-                    distance=models.Distance.COSINE
+        if self.qdrant.collection_exists(collection_name=self.collection) == True:
+            if self.verbose:
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] Collection {self.collection} found.')
+                vectors_count = self.qdrant.get_collection(self.collection).vectors_count
+                points_count = self.qdrant.get_collection(self.collection).points_count
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] self.collection: {self.collection} already exists with {vectors_count} vectors and {points_count} points.')
+        else:
+            try:
+                self.qdrant.create_collection(
+                    collection_name=self.collection,
+                    vectors_config=models.VectorParams(
+                        size=self.encoder.get_sentence_embedding_dimension(),
+                        distance=models.Distance.COSINE
+                    )
                 )
-            )
-            if self.verbose:
-                print(f"created self.collection: {self.collection}")
-        except Exception as e:
-            if self.verbose:
-                vectors_count = self.qdrant.get_collection(
-                    self.collection).vectors_count
                 if self.verbose:
-                    print(
-                        f"self.collection: {self.collection} already exists with {vectors_count} vectors, not creating: {e}")
+                    logger.debug(f'[Memoir] [{self.__class__.__name__}] Created self.collection: {self.collection}')
+            except Exception as e:
+                if self.verbose:
+                    logger.debug(f'[Memoir] [{self.__class__.__name__}] There was an error while creating self.collection: {self.collection}: {e}')
 
     def delete_vector_db(self):
         try:
@@ -69,11 +103,10 @@ class LTM():
                 collection_name=self.collection,
             )
             if self.verbose:
-                print(f"deleted self.collection: {self.collection}")
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] Deleted self.collection: {self.collection}')
         except Exception as e:
             if self.verbose:
-                print(
-                    f"self.collection: {self.collection} does not exists, not deleting: {e}")
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] self.collection: {self.collection} does not exist, not deleting: {e}')
 
     def store(self, doc_to_upsert):
         operation_info = self.qdrant.upsert(
@@ -123,7 +156,7 @@ class LTM():
                 formated_results.append(result.payload['comment'] + ": on " + str(date_str))
             else:
                 if self.verbose:
-                    print("Not adding " + comment)
+                    logger.debug(f"[Memoir] [{self.__class__.__name__}] Not adding {comment}")
             result_count += 1
         return formated_results
 
@@ -143,11 +176,11 @@ class LTM():
                 now = datetime.now()
                 if now - datetime_obj <= timedelta(hours=range):
                     if self.verbose:
-                        print("Adding memory to stm_context")
+                        logger.debug(f"[Memoir] [{self.__class__.__name__}] Adding memory to stm_context")
                     formated_results.append(result.payload['comment'] + ": on " + str(date_str))
             else:
                 if self.verbose:
-                    print("Not adding " + comment)
+                    logger.info(f"[Memoir] [{self.__class__.__name__}] Not adding {comment}")
             result_count += 1
         return formated_results
 
--- rag/rag_data_memory.py
+++ rag/rag_data_memory.py
@@ -8,8 +8,10 @@ Copyright (c) 2024 brucepro
 
 """
 
+import os
 import random
 from datetime import datetime
+from modules.logging_colors import logger
 from qdrant_client import models, QdrantClient
 from qdrant_client.http.models import PointStruct
 from html import escape
@@ -17,48 +19,84 @@ from sentence_transformers import SentenceTransformer
 
 
 class RagDataMemory():
-    def __init__(self,
-                 collection,
-                 ltm_limit,
-                 verbose=False,
-                 embedder='all-mpnet-base-v2',
-                 address='localhost',
-                 port=6333
-                 ):
+    def __init__(
+        self,
+        collection,
+        ltm_limit,
+        verbose=False,
+        embedder='all-mpnet-base-v2',
+        address='localhost',
+        port=6333,
+        device='cuda'
+    ):
 
         self.verbose = verbose
         if self.verbose:
-            print("initiating verbose debug mode.............")
+            logger.debug(f'[Memoir] [{self.__class__.__name__}] Verbose mode enabled.')
         self.collection = collection + "_rag_data"
         self.ltm_limit = ltm_limit
-        print("RAG LIMIT:" + str(ltm_limit))
+        if self.verbose:
+            logger.debug(f"[Memoir] [{self.__class__.__name__}] LIMIT: {str(ltm_limit)}")
         self.address = address
         self.port = port
         if self.verbose:
-            print(f"addr:{self.address}, port:{self.port}")
+            logger.debug(f'[Memoir] [{self.__class__.__name__}] [QDRANT] addr:{self.address}, port:{self.port}')
+
         self.embedder = embedder
-        self. encoder = SentenceTransformer(self.embedder)
+        self.device = device
+        # Check if specified embedder is already downloaded to load it from file
+        filePath = os.path.dirname(os.path.realpath(__file__))
+        basePath = os.path.abspath(os.path.join(filePath, os.pardir))
+        embedderSavePath = os.path.join(
+            basePath + os.sep,
+            "storage" + os.sep,
+            "models" + os.sep,
+            self.embedder
+        )
+        embedderSaveFile = os.path.join(
+            embedderSavePath + os.sep,
+            "model.safetensors"
+        )
+        if os.path.exists(embedderSavePath) and os.path.isfile(embedderSaveFile):
+            if self.verbose:
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] embedder {self.embedder} found in {embedderSavePath}, will try to load from disk.')
+            try:
+                self.encoder = SentenceTransformer(embedderSavePath)  # , device=self.device)
+            except Exception as e:
+                logger.error(f'[Memoir] [{self.__class__.__name__}] There was an error while loading {self.embedder}, please check.')
+        else:
+            if self.verbose:
+                logger.warning(f'[Memoir] [{self.__class__.__name__}] embedder {self.embedder} NOT FOUND in {embedderSavePath}, will be downloaded and saved to disk.')
+            self.encoder = SentenceTransformer(self.embedder)  # , device=self.device)
+            self.encoder.save(embedderSavePath)
+
         self.qdrant = QdrantClient(self.address, port=self.port)
         self.create_vector_db_if_missing()
 
     def create_vector_db_if_missing(self):
-        try:
-            self.qdrant.create_collection(
-                collection_name=self.collection,
-                vectors_config=models.VectorParams(
-                    size=self.encoder.get_sentence_embedding_dimension(),
-                    distance=models.Distance.COSINE
+        if self.qdrant.collection_exists(collection_name=self.collection) == True:
+            if self.verbose:
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] Collection {self.collection} found.')
+                vectors_count = self.qdrant.get_collection(self.collection).vectors_count
+                points_count = self.qdrant.get_collection(self.collection).points_count
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] self.collection: {self.collection} already exists with {vectors_count} vectors and {points_count} points.')
+        else:
+            try:
+                self.qdrant.create_collection(
+                    collection_name=self.collection,
+                    vectors_config=models.VectorParams(
+                        size=self.encoder.get_sentence_embedding_dimension(),
+                        distance=models.Distance.COSINE
+                    )
                 )
-            )
-            if self.verbose:
-                print(f"created self.collection: {self.collection}")
-        except Exception as e:
-            if self.verbose:
-                vectors_count = self.qdrant.get_collection(
-                    self.collection).vectors_count
                 if self.verbose:
-                    print(
-                        f"self.collection: {self.collection} already exists with {vectors_count} vectors, not creating: {e}")
+                    logger.debug(f'[Memoir] [{self.__class__.__name__}] created self.collection: {self.collection}')
+            except Exception as e:
+                if self.verbose:
+                    vectors_count = self.qdrant.get_collection(
+                        self.collection).vectors_count
+                    if self.verbose:
+                        logger.debug(f'[Memoir] [{self.__class__.__name__}] There was an error while creating self.collection: {self.collection}: {e}')
 
     def delete_vector_db(self):
         try:
@@ -66,11 +104,10 @@ class RagDataMemory():
                 collection_name=self.collection,
             )
             if self.verbose:
-                print(f"deleted self.collection: {self.collection}")
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] deleted self.collection: {self.collection}')
         except Exception as e:
             if self.verbose:
-                print(
-                    f"self.collection: {self.collection} does not exists, not deleting: {e}")
+                logger.debug(f'[Memoir] [{self.__class__.__name__}] self.collection: {self.collection} does not exist, not deleting: {e}')
 
     def store(self, doc_to_upsert):
         operation_info = self.qdrant.upsert(
@@ -79,7 +116,7 @@ class RagDataMemory():
             points=self.get_embedding_vector(doc_to_upsert),
         )
         if self.verbose:
-            print(operation_info)
+            logger.debug(f'[Memoir] [{self.__class__.__name__}] {operation_info}')
 
     def get_embedding_vector(self, doc):
         data = doc['comment']
@@ -120,7 +157,7 @@ class RagDataMemory():
                 formated_results.append(result.payload['comment'] + ": on " + str(date_str))
             else:
                 if self.verbose:
-                    print("Not adding " + comment)
+                    logger.debug(f"[Memoir] [{self.__class__.__name__}] Not adding {comment}")
             result_count += 1
         return formated_results
 
